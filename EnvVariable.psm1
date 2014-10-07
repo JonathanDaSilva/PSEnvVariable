452912f1c0f550a3b714060ab7699fe5ad570105
Jonathan Da Silva <dasilvabastos.jonathan@gmail.com>
2014-10-07 18:24:23 +0200

    Start.

---
+++ EnvVariable.psm1
@@ -0,0 +1,85 @@
+function Set-Env
+{
+  param (
+    [Parameter(Mandatory=$True,Position=1)]
+    [String]$name  = "",
+    [Parameter(Mandatory=$True,Position=2)]
+    [String]$value = ""
+  )
+
+  $path = $("Env:\"+$name)
+  if(-not $(Test-Path $path)) {
+    Set-Content $path $value
+  } else {
+    Set-Content $path $value
+  }
+  [Environment]::SetEnvironmentVariable($name, $value, [System.EnvironmentVariableTarget]::Machine)
+
+  Write-Host "$($path) = $($value)" -ForegroundColor "green"
+}
+
+function Remove-Env
+{
+  param (
+    [Parameter(Mandatory=$True,Position=1)]
+    [String]$name = ""
+  )
+  $path = "Env:\"+$name
+  if(-not $(Test-Path $path)) {
+    Write-Host "$($path) :: Doesn't exist" -ForegroundColor "red"
+  } else {
+    Remove-Item $path -Force
+  }
+  [Environment]::SetEnvironmentVariable($name, $value, [System.EnvironmentVariableTarget]::Machine)
+}
+
+function Add-Path
+{
+  param (
+    [Parameter(Mandatory=$False,Position=1)]
+    [Object]$dir = $(Get-Location)
+  )
+  if(-not (Test-InPath $dir)) {
+    $env:Path += ";$($dir)"
+    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    Write-Host "$($dir) :: Added to Path" -ForegroundColor "green"
+  } else {
+    Write-Host "$($dir) :: Already in Path" -ForegroundColor "red"
+  }
+}
+
+function Remove-Path
+{
+  param (
+    [Parameter(Mandatory=$False,Position=1)]
+    [Object]$dir = $(Get-Location)
+  )
+  if(Test-InPath) {
+    $env:Path = $env:Path.replace($dir, '')
+    $env:Path = $env:Path.replace(';;', ';')
+    [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    Write-Host "$($dir) :: Deleted for the path" -ForegroundColor "green"
+  } else {
+    Write-Host "$($dir) :: Is not in the path" -ForegroundColor "red"
+  }
+}
+
+function Test-InPath
+{
+  param(
+    [Parameter(Mandatory=$False,Position=1)]
+    [Object]$dir = $(Get-Location)
+  )
+  foreach($path in $env:Path.split(';')) {
+    if($path -eq $dir) {
+      return $TRUE
+    }
+  }
+  return $FALSE
+}
+
+Export-ModuleMember Set-Env
+Export-ModuleMember Remove-Env
+Export-ModuleMember Add-Path
+Export-ModuleMember Remove-Path
+Export-ModuleMember Test-InPath
